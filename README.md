0d9bc1bc9d4a2989532daaf754b3f249dc2374e3
dhoskin <dhoskin@localhost>
2013-12-03 20:31:59 -0800

    Add minimal httpd magic plugin.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Weebsocket
 
 Weebsocket is a WebSocket server for Plan 9.
+
+## Installing
+
+Currently this is a bit of a hack.
+
+    cp minimal.c /sys/src/cmd/ip/httpd
+    cd /sys/src/cmd/ip/httpd
+    # add "minimal" to TARG in mkfile
+    mk install
+
+## Testing
+
+Visit `http://my-server.example.com/magic/minimal` in your favourite
+web browser.
---
+++ minimal.c
@@ -0,0 +1,38 @@
+#include <u.h>
+#include <libc.h>
+#include <bio.h>
+#include "httpd.h"
+#include "httpsrv.h"
+
+Hio *ho;
+
+int
+dostuff(HConnect *c)
+{
+	if(strcmp(c->req.meth, "GET"))
+		return hunallowed(c, "GET");
+
+	hokheaders(c);
+	hprint(ho, "Content-type: text/html\r\n");
+	hprint(ho, "\r\n");
+
+	hprint(ho, "<html><body>\r\n"
+		"<h1>whee, httpd <i>magic</i> is working!</h1>\r\n"
+		"<p>Lorem ipsum and so on.  I can eat glass!\r\n"
+		"</body></html>\r\n");
+
+	hflush(ho);
+	return 1;
+}
+
+void
+main(int argc, char **argv)
+{
+	HConnect *c;
+
+	c = init(argc, argv);
+	ho = &c->hout;
+	if(hparseheaders(c, HSTIMEOUT) >= 0)
+		dostuff(c);
+	exits(nil);
+}
