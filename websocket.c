9c962a162c6ca825fd9c87451c1c5f4a187d7e25
dhoskin <dhoskin@localhost>
2013-12-10 22:26:42 -0800

    Implement most Websocket headers.  Still need to handle nonce hashing and subprotocol selection.

--- websocket.c
+++ websocket.c
@@ -8,6 +8,9 @@ Hio *ho;
 
 #define MAXHDRS 64
 
+const char wsnoncekey[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+const int wsversion = 13;
+
 HSPairs *
 parseheaders(char *headers)
 {
@@ -54,12 +57,12 @@ cleanup:
 	return nil;
 }
 
-HSPairs *
+char *
 getheader(HSPairs *h, const char *k)
 {
 	for(; h != nil; h = h->next)
 		if(!cistrcmp(h->s, k))
-			return h;
+			return h->t;
 	return nil;
 }
 
@@ -81,33 +84,76 @@ failhdr(HConnect *c, int code, const char *status, const char *message)
 	return 0;
 }
 
-int
-dostuff(HConnect *c)
+void
+okhdr(HConnect *c, const char *wshashedkey, const char *proto)
 {
-	HSPairs *hdrs, *h;
+	Hio *o;
+	o = &c->hout;
+	hprint(o, "%s 101 Switching Protocols\r\n", hversion);
+	hprint(o, "Upgrade: websocket\r\n");
+	hprint(o, "Connection: upgrade\r\n");
+	hprint(o, "Sec-WebSocket-Accept: %s\r\n", wshashedkey);
+	if(proto)
+		hprint(o, "Sec-WebSocket-Protocol: %s\r\n", proto);
+	/* we don't handle extensions */
+	hprint(o, "\r\n");
+	hflush(o);
+}
+
+int
+testwsversion(const char *vs)
+{
+	int i, n;
+	char *v[16];
+
+	n = getfields(vs, v, 16, 1, "\t ,");
+	for(i = 0; i < n; ++i)
+		if(atoi(v[i]) == wsversion)
+			return 1;
+	return 0;
+}
+
+int
+dowebsock(HConnect *c)
+{
+	HSPairs *hdrs;
+	char *s, *wsclientkey;
+	char *rawproto;
+	char *wshashedkey, *proto;
+	uchar wsclientkeydec[16];
 
 	if(strcmp(c->req.meth, "GET"))
 		return hunallowed(c, "GET");
 
 	//return failhdr(c, 403, "Forbidden", "my hair is on fire");
 
-	hokheaders(c);
-	hprint(ho, "Content-type: text/html\r\n");
-	hprint(ho, "\r\n");
-
-	hprint(ho, "<html><body>\r\n"
-		"<h1>whee, httpd <i>magic</i> is working!</h1>\r\n"
-		"<p>Lorem ipsum and so on.  I can eat glass!\r\n");
-
 	hdrs = parseheaders((char *)c->header);
-	hprint(ho, "<table>\n");
-	for(h = hdrs; h != nil; h = h->next){
-		hprint(ho, "\t<tr><td>%s</td><td>%s</td></tr>\n", h->s, h->t);
-	}
-	hprint(ho, "</table>\n");
 
-	hprint(ho, "<b>Connection:</b> <tt>%s</tt>\n", getheader(hdrs, "connection")->t);
-	hprint(ho, "</body></html>\r\n");
+	s = getheader(hdrs, "upgrade");
+	if(!s || !cistrstr(s, "websocket"))
+		return failhdr(c, 400, "Bad Request", "no <pre>upgrade: websocket</pre> header.");
+	s = getheader(hdrs, "connection");
+	if(!s || !cistrstr(s, "upgrade"))
+		return failhdr(c, 400, "Bad Request", "no <pre>connection: upgrade</pre> header.");
+	wsclientkey = getheader(hdrs, "sec-websocket-key");
+	if(!wsclientkey || dec64(wsclientkeydec, 16, wsclientkey, strlen(s)) != 16)
+		return failhdr(c, 400, "Bad Request", "invalid websocket nonce key.");
+	s = getheader(hdrs, "sec-websocket-version");
+	if(!s || !testwsversion(s))
+		return failhdr(c, 426, "Upgrade Required", "could not match websocket version.");
+	/* XXX should get resource name */
+	rawproto = getheader(hdrs, "sec-websocket-protocol");
+	proto = nil;
+	/* XXX should test if proto is acceptable" */
+	/* should get sec-websocket-extensions */
+
+	/* OK, we seem to have a valid Websocket request. */
+
+	/* XXX should hash websocket key! */
+
+	okhdr(c, wshashedkey, proto);
+
+	/* We should now have an open Websocket connection. */
 
 	hflush(ho);
 	return 1;
@@ -121,6 +167,6 @@ main(int argc, char **argv)
 	c = init(argc, argv);
 	ho = &c->hout;
 	if(hparseheaders(c, HSTIMEOUT) >= 0)
-		dostuff(c);
+		dowebsock(c);
 	exits(nil);
 }
